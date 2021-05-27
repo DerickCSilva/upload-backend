95a79f3b6fbe23db1bceb8c802d974e9b3148690
DerickCSilva <derick.cardoso17@gmail.com>
2021-05-26 21:38:23 -0300

    first comiit

---
+++ config/mongodb.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+mongoose.connect(
+    'mongodb+srv://deploy:uploaddeploy@cluster0.wdd3a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true
+    }
+)
+    .catch(e => {
+        const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
+        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
+    })
